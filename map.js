51f6067c262a7ca5bb0966b3ec42aa12bea7efc7
TanishaKr07 <tanishakr@gmail.com>
2025-11-15 16:14:57 -0800

    input blue bikes csv url

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 console.log('Mapbox GL JS Loaded:', mapboxgl);
@@ -49,4 +50,18 @@ map.on('load', async () => {
   'line-opacity': 0.6       // Slightly less transparent
 },
 });
+  let jsonData;
+  try {
+    const jsonurl = INPUT_BLUEBIKES_CSV_URL;
+
+    // Await JSON fetch
+    const jsonData = await d3.json(jsonurl);
+
+    console.log('Loaded JSON Data:', jsonData); // Log to verify structure
+  } catch (error) {
+    console.error('Error loading JSON:', error); // Handle errors
+  }
 });
+
+let stations = jsonData.data.stations;
+console.log('Stations Array:', stations); // Log to verify structure
